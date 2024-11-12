4f159411a0aa576645f811eaf79194d609707cd9
netocalegari <netokalegari@gmail.com>
2024-11-12 12:25:49 -0300

    chore: test

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { app } from "./app";
 import { env } from "./env";
 
 app
-  .listen({ port: env.PORT })
+  .listen({ port: env.PORT, host: "0.0.0.0" })
   .then(() => console.log(`Running on port ${env.PORT}`));
